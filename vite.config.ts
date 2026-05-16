b5f284472d543115507b58c24f0c317a50576c0f
Dukwhan Kim <ddukki@gmail.com>
2026-05-16 12:51:08 -0500

    feat: configure Vite for multi-page build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: 'dist-demo' },
+  build: {
+    outDir: 'dist-demo',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        editor: 'editor/index.html',
+      },
+    },
+  },
 })
