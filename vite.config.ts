aefb9012beaed337593dd300715e16569088cdcd
Dukwhan Kim <ddukki@gmail.com>
2026-05-16 13:11:43 -0500

    chore: add dev server URL redirect middleware

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'editor-redirect',
+      configureServer(server) {
+        server.middlewares.use((req, _res, next) => {
+          if (req.url === '/editor' || req.url === '/editor/') {
+            req.url = '/editor/index.html'
+          }
+          next()
+        })
+      },
+    },
+  ],
   build: {
     outDir: 'dist-demo',
     rollupOptions: {
