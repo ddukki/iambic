45578214e4510c70908a5cc8e0af70039e7b55f4
Dukwhan Kim <ddukki@gmail.com>
2026-05-16 20:55:09 -0500

    feat: add subpath exports to package.json
    Remove editor/ directory. Add subpath exports (./dom, ./css, ./layout,
    ./html, ./schema, ./validator). Simplify vite.config to single-page.

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'editor-redirect',
-      configureServer(server) {
-        server.middlewares.use((req, _res, next) => {
-          if (req.url === '/editor' || req.url === '/editor/') {
-            req.url = '/editor/index.html'
-          }
-          next()
-        })
-      },
-    },
-  ],
+  plugins: [react()],
   build: {
     outDir: 'dist-demo',
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        editor: 'editor/index.html',
-      },
-    },
   },
 })
